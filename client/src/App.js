6a6a6b2379e7aba4aeae210ea59602db2f6e47a4
erickmuriithi <rickymurithii@gmail.com>
2022-12-10 18:24:15 +0300

    add

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    fetch("/movies")
+      .then((r) => r.json())
+      .then((movies) => console.log(movies));
+  }, []);
+/*useEffect to fetch db*/
+/*setting up a proxy in the top level of package json and update start script PORT 3000"*/
+/*Or installing gem foreman and creating a Procfile.dev file for apis*/
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
